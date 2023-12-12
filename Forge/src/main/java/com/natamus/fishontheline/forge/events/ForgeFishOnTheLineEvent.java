c580c6497d12ec7c34a05b22bc4ebaae8069b0e9
Rick South <rick@natamus.com>
2023-12-12 10:13:20 +0100

    Fish On The Line source code for 1.20.1.

---
+++ Forge/src/main/java/com/natamus/fishontheline/forge/events/ForgeFishOnTheLineEvent.java
@@ -0,0 +1,25 @@
+package com.natamus.fishontheline.forge.events;
+
+import com.natamus.fishontheline.events.FishOnTheLineEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.TickEvent.Phase;
+import net.minecraftforge.event.TickEvent.PlayerTickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class ForgeFishOnTheLineEvent {
+	@SubscribeEvent
+	public void onPlayerTick(PlayerTickEvent e) {
+		Player player = e.player;
+		Level level = player.level();
+		if (level.isClientSide || !e.phase.equals(Phase.START)) {
+			return;
+		}
+
+		FishOnTheLineEvent.onPlayerTick((ServerLevel)level, (ServerPlayer)player);
+	}
+}
