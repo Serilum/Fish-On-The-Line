87190e7f45c6dce800cc4b8785d98ed05cdeb568
Rick South <rick@natamus.com>
2023-12-12 10:13:29 +0100

    Fish On The Line source code for 1.20.3.

---
+++ Fabric/src/main/java/com/natamus/fishontheline/ModFabric.java
@@ -0,0 +1,32 @@
+package com.natamus.fishontheline;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.fabric.callbacks.CollectivePlayerEvents;
+import com.natamus.fishontheline.events.FishOnTheLineEvent;
+import com.natamus.fishontheline.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		CollectivePlayerEvents.PLAYER_TICK.register((ServerLevel world, ServerPlayer player) -> {
+			FishOnTheLineEvent.onPlayerTick(world, player);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
