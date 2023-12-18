3e457ce71c14491875411b2d9a59580d5578d814
Rick South <rick@natamus.com>
2023-12-18 10:28:32 +0100

    (v3.2) Improved mod icon compression, which lowers the jar file size. [+2]

--- Common/src/main/java/com/natamus/fishontheline/util/Reference.java
+++ Common/src/main/java/com/natamus/fishontheline/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.fishontheline.util;
 public class Reference {
 	public static final String MOD_ID = "fishontheline";
 	public static final String NAME = "Fish On The Line";
-	public static final String VERSION = "3.1";
+	public static final String VERSION = "3.2";
 	public static final String ACCEPTED_VERSIONS = "[1.20.4]";
 }
