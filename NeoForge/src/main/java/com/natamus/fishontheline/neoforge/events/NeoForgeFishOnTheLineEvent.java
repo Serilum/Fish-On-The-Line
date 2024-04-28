a6817daf7175f2375d8da627434f2f6e678c51f7
Rick South <rick@natamus.com>
2024-04-28 16:02:53 +0200

    Fish On The Line source code for 1.20.5.

---
+++ NeoForge/src/main/java/com/natamus/fishontheline/neoforge/events/NeoForgeFishOnTheLineEvent.java
@@ -0,0 +1,25 @@
+package com.natamus.fishontheline.neoforge.events;
+
+import com.natamus.fishontheline.events.FishOnTheLineEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.event.TickEvent.Phase;
+import net.neoforged.neoforge.event.TickEvent.PlayerTickEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeFishOnTheLineEvent {
+	@SubscribeEvent
+	public static void onPlayerTick(PlayerTickEvent e) {
+		Player player = e.player;
+		Level level = player.level();
+		if (level.isClientSide || !e.phase.equals(Phase.START)) {
+			return;
+		}
+
+		FishOnTheLineEvent.onPlayerTick((ServerLevel)level, (ServerPlayer)player);
+	}
+}
