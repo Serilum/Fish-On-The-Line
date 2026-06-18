cc6b6d660e7a66368e603890c73f59865deeffec
Rick South <rick@natamus.com>
2026-06-18 23:54:04 +0200

    Updated Fish On The Line to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/fishontheline/util/Reference.java
+++ Common/src/main/java/com/natamus/fishontheline/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "fishontheline";
 	public static final String NAME = "Fish On The Line";
 	public static final String VERSION = "3.5";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
